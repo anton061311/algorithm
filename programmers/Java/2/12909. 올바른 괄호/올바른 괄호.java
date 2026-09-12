8c09836a86ee2928e5252f3d6cdd1ab7b99f3416
Leo Lee <anton061311@gmail.com>
2026-09-12 15:40:55 +0900

    [level 2] Title: 올바른 괄호, Time: 10.14 ms, Memory: 58 MB -BaekjoonHub

---
+++ programmers/Java/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0 ; i < s.length(); i++){
+            if (s.charAt(i) == ')'){
+                if(stack.isEmpty()) return false;
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+			}
+        }
+        
+        if(!stack.isEmpty()) return false;
+
+        return true;
+    }
+}
